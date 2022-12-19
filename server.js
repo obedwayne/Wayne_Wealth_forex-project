83a5edac00b1ba3d05c83e2257e83246dbc39426
Clement Bogyah <Zena@Clements-MacBook-Pro.local>
2022-12-19 16:41:58 +0000

    file

--- server.js
+++
@@ -1,8 +0,0 @@
-// const dotenv = require("dotenv");
-const app = require("./server/app");
-
-const port = process.env.PORT;
-
-const server = app.listen(port, () => {
-  console.log(`server is live on ${port}`);
-});
